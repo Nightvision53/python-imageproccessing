a24ec7aa04866222e620bb3bf4cfbe864c98b6a7
nightvision <babalibrahim34@gmail.com>
2021-12-05 16:29:09 +0300

    Functions defined.

---
+++ main.py
@@ -0,0 +1,70 @@
+def generate_random(row, column):
+    pass
+
+
+def is_valid(img):
+    pass
+
+
+def read_from_file(filename):
+    pass
+
+
+def write_to_file(img, filename):
+    pass
+
+
+def clear(img):
+    pass
+
+
+def set_value(img, value, channel='rgb'):
+    pass
+
+
+def fix(img):
+    pass
+
+
+def rotate90(img):
+    pass
+
+
+def rotate180(img):
+    pass
+
+
+def rotate270(img):
+    pass
+
+
+def mirror_x(img):
+    pass
+
+
+def mirror_y(img):
+    pass
+
+
+def enhance(img, value, channel='rgb'):
+    pass
+
+
+def grayscale(img, mode=1):
+    pass
+
+
+def get_freq(img, channel='rgb', bin_size=16):
+    pass
+
+
+def scale_down(img, N):
+    pass
+
+
+def scale_up(img, N):
+    pass
+
+
+def apply_window(img, window):
+    pass
